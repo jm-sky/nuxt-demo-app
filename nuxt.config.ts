e4e264a38f4628dc71c3f2589fec5f8feac58c1a
Jan Madeyski <jan.madeyski@sklodowscy.pl>
2024-04-25 12:32:53 +0200

    eslint & i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/ui"],
+  modules: ["@nuxt/eslint", "@nuxt/ui", "@nuxtjs/i18n"],
   tailwindcss: {
     configPath: 'tailwind.config',
+  },
+  eslint: {
+    config: {
+      stylistic: true,
+    },
   },
 })
